29dd59c52777ea41cfcb8f197c4facee512ddba3
Jared Conover <jconover@vendere.ca>
2021-08-03 13:44:17 -0400

    Initial Commit

---
+++ HWS Storm Viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS Storm Viewer
+//
+//  Created by Jared Conover on 2021-08-03.
+//  Copyright © 2021 Jared Conover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
