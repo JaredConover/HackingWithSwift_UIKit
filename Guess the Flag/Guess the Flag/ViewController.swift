46e9f523688f089b5c6c1faa55316a0359db52e6
Jared Conover <jconover@vendere.ca>
2021-08-21 15:56:46 -0400

    02 - Guess the Flag - Add button action, show alert controller, shuffle the array of countries, evaluate score

--- Guess the Flag/Guess the Flag/ViewController.swift
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -13,9 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var Button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     var playerScore = 0
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,18 +29,41 @@ class ViewController: UIViewController {
         Button1.layer.borderColor = UIColor.lightGray.cgColor
         Button2.layer.borderColor = UIColor.lightGray.cgColor
         Button3.layer.borderColor = UIColor.lightGray.cgColor
-            
-            countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        
+        countries.shuffle()
+        
         Button1.setImage(UIImage(named: countries[0]), for: .normal)
         Button2.setImage(UIImage(named: countries[1]), for: .normal)
         Button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        
+        title = countries[correctAnswer].uppercased()
+        
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            playerScore += 1
+        } else {
+            title = "Wrong"
+            playerScore -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(playerScore)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
+    
 }
 
