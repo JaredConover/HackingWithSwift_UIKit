54f375713c9ecdbfe1d6dfdbbdc2872382139998
Jared Conover <jconover@vendere.ca>
2021-08-21 14:38:50 -0400

    02 - Guess the Flag - Initial setup of flags, Button styling of border and bordercolor with CALayer

--- Guess the Flag/Guess the Flag/ViewController.swift
+++ Guess the Flag/Guess the Flag/ViewController.swift
@@ -12,9 +12,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var Button2: UIButton!
     @IBOutlet weak var Button3: UIButton!
     
+    var countries = [String]()
+    var playerScore = 0
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // This modifies the CALayer property that sits beneath all UIViews
+        Button1.layer.borderWidth = 1
+        Button2.layer.borderWidth = 1
+        Button3.layer.borderWidth = 1
+        
+        // The CALayer doesnt understand UIColor so we need to convert to cgColor
+        Button1.layer.borderColor = UIColor.lightGray.cgColor
+        Button2.layer.borderColor = UIColor.lightGray.cgColor
+        Button3.layer.borderColor = UIColor.lightGray.cgColor
+            
+            countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        Button1.setImage(UIImage(named: countries[0]), for: .normal)
+        Button2.setImage(UIImage(named: countries[1]), for: .normal)
+        Button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
